5b170e17030cb02c914ee7af553306051b155a19
BitXplorer <70508247+BitXplorer@users.noreply.github.com>
2020-12-01 16:55:02 +0100

    Standard Game code, more tests.

--- Test/BackEndTest.java
+++ Test/BackEndTest.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Repeatable;
 public class BackEndTest {
     BackEnd dTest = new BackEnd();
 
-    @Test()
+    @Test
     public void throwDiceTest() {
         for(int i = 0; i<1000; i++) {
             dTest.throwDice();
@@ -24,6 +24,32 @@ public class BackEndTest {
         System.out.println(roundCounter);
         Assert.assertTrue(roundCounter != 0 && roundCounter <=3);
         dTest.addScore();
+    }
+    @Test
+    public void checkRoundCounterAfterAddScore() {
+        dTest.throwDice();
+        dTest.throwDice();
+        dTest.addScore();
+        int roundCounter = dTest.getRoundCounter();
+        System.out.println(roundCounter);
+        Assert.assertTrue(roundCounter == 0);
+    }
+    @Test
+    public void checkDiceAfterAddScore() {
+        dTest.throwDice();
+        dTest.addScore();
+        int dice = dTest.getDice();
+        System.out.println(dice);
+        Assert.assertTrue(dice == 0);
+    }
+    @Test
+    public void checkGameOverAfterAddScore100Points() {
+        dTest.setP1Score(99);
+        dTest.setP2Score(99);
+        dTest.addScore();
+        boolean gameOver = dTest.gameOver();
+        System.out.println(gameOver);
+        Assert.assertTrue(gameOver);
     }
 
 
