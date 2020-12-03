b055adfe2dfe191d1bd374d016f03a613fe10cdd
Joel Reboia <70518069+J-Reboia@users.noreply.github.com>
2020-12-03 16:09:01 +0100

    Ytterligare tester gjorda

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
+import org.junit.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        FrontEnd f = new FrontEnd();
+        //FrontEnd f = new FrontEnd();
+        String defaultTempDir = System.getProperty("java.io.tmpdir");
+        PrintWriter out;
+        Scanner fileScanner;
+        try {
+            File temp = File.createTempFile("temp", ".txt");
+            out = new PrintWriter(temp);
+            out.print("Lorem ipsum");
+            out.println("dolor sit amet.");
+            out.close();
+            fileScanner = new Scanner(temp);
+            while (fileScanner.hasNextLine()) {
+                String s = fileScanner.nextLine();
+                System.out.print(s);
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/IqStringHandlerTest.java
+++ src/test/IqStringHandlerTest.java
@@ -1,10 +1,16 @@
 import junit.framework.TestCase;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.rules.TemporaryFolder;
 
-import java.security.spec.ECField;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 
 public class IqStringHandlerTest extends TestCase {
+
 
     public void testCountVowels1() {
         //fixme funkar ej med versaler
@@ -51,6 +57,49 @@ public class IqStringHandlerTest extends TestCase {
     public void testInvert3() {
         Assert.assertEquals("10000100 10000110 00110110 11110010 00000100 01010110 10100110 00010010",
                 Iqh.invert("01001000 01100101 01101010 00100000 01001111 01101100 01100001 00100001"));
+    }
+
+    public void testLowerHalf1() {
+        Assert.assertEquals("One", Iqh.lowerHalf("OneTwo"));
+    }
+
+    public void testLowerHalf2() {
+        Assert.assertNull(Iqh.lowerHalf("One Two"));
+    }
+
+    public void testLowerHalf3() {
+        Assert.assertEquals("@F_1",Iqh.lowerHalf("@F_1337!"));
+    }
+
+    public void testUpperHalf1() {
+        Assert.assertEquals("Two",Iqh.upperHalf("OneTwo"));
+    }
+
+    public void testUpperHalf2() {
+        Assert.assertNull(Iqh.upperHalf("Seven"));
+    }
+
+    public void testReadString1() {
+        //String defaultTempDir = System.getProperty("java.io.tmpdir");
+        PrintWriter out;
+        Scanner fileScanner;
+        String s = "";
+        try {
+            File temp = File.createTempFile("temp", ".txt");
+            out = new PrintWriter(temp);
+            out.print("Lorem ipsum");
+            out.println("dolor sit amet.");
+            out.close();
+            fileScanner = new Scanner(temp);
+            while (fileScanner.hasNextLine()) {
+                s = fileScanner.nextLine();
+            }
+            Assert.assertEquals(s,Iqh.readString(temp.getAbsolutePath()));//fixme fungerar nu men behöver ordna att filen alltid raderas
+            temp.deleteOnExit();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     @After
     public void tD() throws Exception {
